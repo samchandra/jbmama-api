ebb9c1cd75311d3420286fa7a62f562ff08687e3
Samuel Chandra <code@samuelc.com>
2017-12-17 16:28:24 +0800

    added dockerfile for deployment

--- main.go
+++ main.go
@@ -1,15 +1,17 @@
 package main
 
+import "net/http"
 import "github.com/gin-gonic/gin"
+
 
 func main() {
 	r := gin.Default()
 
 	// Ping test
 	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "pong"})
+		c.JSON(http.StatusOK, gin.H{"message": "pong"})
 	})
 
 	// Listen and Server in 0.0.0.0:8081
-	r.Run(":8081")
+	r.Run(":9081")
 }
